f6ff70a86368f096ff537cce2d3a2e2ea3a894ca
terryyeng-ux <yengterry@gmail.com>
2021-06-05 13:58:09 +0800

    secret-word-app

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
+var randomWord;
+
+var winCount = 0;
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue;
+
+  var remainingWins = 2 - winCount;
+
+  myOutputValue =
+    "You guessed incorrectly! You need " +
+    remainingWins +
+    " correct guesses to win";
+
+  var randomNumber = function () {
+    var randomDecimal = Math.random() * 3;
+    var randomInt = Math.floor(randomDecimal);
+    var randomNo = randomInt + 1;
+    return randomNo;
+  };
+
+  randomNo = randomNumber();
+
+  if (randomNo == 1) randomWord = "banana";
+  if (randomNo == 2) randomWord = "chisel";
+  if (randomNo == 3) randomWord = "faucet";
+  console.log(randomWord);
+  console.log(input);
+
+  if (input == randomWord) {
+    winCount = winCount + 1;
+    remainingWins = 2 - winCount;
+    myOutputValue =
+      "You guessed correctly! You need " +
+      remainingWins +
+      " correct guesses to win";
+    return myOutputValue;
+  }
+
+  if (remainingWins == 0) {
+    myOutputValue = "You win!";
+    return myOutputValue;
+  }
   return myOutputValue;
 };
